b815d4960389389f0776e33094a7ec03c838e0e8
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2024-04-02 16:06:44 +0300

    testit menee läpi

--- backend/routes/student.js
+++ backend/routes/student.js
@@ -33,7 +33,7 @@ router.post('/',function(request, response){
         }
         else{
             //response.json(result);
-            response.json(result);
+            response.json(result.affectedRows);
         }
     });
 });
